5ec37b89400322f2203fe152bef4ba454f9825dc
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-12-10 11:16:41 -0600

    Add: Dto para actualiza y crear, paginacion y excepciones

---
+++ src/books/dto/create-book.dto.ts
@@ -0,0 +1,34 @@
+
+import { Type } from "class-transformer";
+import { IsNumber, IsPositive, IsString } from "class-validator";
+
+export class CreateBookDto {
+
+   @IsString()
+   public isbn: string
+
+   @IsString()
+   public titulo: string
+
+   @IsString()
+   public tipo: string
+
+   @IsString()
+   public autor: string
+
+   @IsNumber()
+   @IsPositive()
+   @Type(() => Number)
+   public status: number
+
+   @IsNumber()
+   @IsPositive()
+   @Type(() => Number)
+   public genero: number
+
+   @IsNumber()
+   @IsPositive()
+   @Type(() => Number)
+   public categoria: number
+
+}
